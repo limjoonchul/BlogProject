9e3aa3449015dd1947e7441d36af5f7a1e1bb6ee
limjoonchul <dlawnscjf511@naver.com>
2020-12-19 15:45:01 +0900

    feat: Post Model

--- src/main/java/com/nalim/model/Post.java
+++ src/main/java/com/nalim/model/Post.java
@@ -1,4 +1,42 @@
 package com.nalim.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
 public class Post {
+    @Id @GeneratedValue
+    private Long seq;
+
+    @Column(nullable = false)
+    private String title;
+
+    private String content;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date creaDate;
+
+    @ManyToOne
+    @JoinColumn(name = "Member_seq")
+    private Member member;
+
+    @OneToMany
+    @JoinColumn(name = "Category_seq")
+    private List<Category> categoryList = new ArrayList<>();
+
+    @ManyToOne
+    @JoinColumn(name = "Blog_seq")
+    private Blog blog;
+
+    @OneToMany(mappedBy = "post", fetch = FetchType.EAGER)  // comment에서 post를 참조하는 필드명
+    @JoinColumn(name = "Comment_seq")
+    List<Comment> commentList = new ArrayList<>();
+
 }
