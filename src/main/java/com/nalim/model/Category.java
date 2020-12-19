e4b34e232f3c11778e35d668c3d462f82b7877a3
limjoonchul <dlawnscjf511@naver.com>
2020-12-19 14:54:10 +0900

    feat: Category Model

--- src/main/java/com/nalim/model/Category.java
+++ src/main/java/com/nalim/model/Category.java
@@ -1,4 +1,21 @@
 package com.nalim.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Setter
+@Getter
+@Entity
+@Table(uniqueConstraints = @UniqueConstraint(columnNames = "Category_name"))
 public class Category {
+    @Id
+    private Long seq;
+    
+    @Column(name = "Category_name")
+    private String name;
+
+    @Lob
+    private byte[] logo;
 }
