a4d0ed2bd0fbd362945ee6e6464e398cf4bc3935
limjoonchul <dlawnscjf511@naver.com>
2020-12-19 15:15:28 +0900

    feat: Comment model

--- src/main/java/com/nalim/model/Comment.java
+++ src/main/java/com/nalim/model/Comment.java
@@ -1,4 +1,28 @@
 package com.nalim.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Getter
+@Setter
+@Entity
 public class Comment {
+    @Id
+    private Long seq;
+
+    private String content;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createDate;
+
+    @ManyToOne
+    @JoinColumn(name = "Member_seq")
+    private Member member;
+
+    @ManyToOne
+    @JoinColumn(name = "Post_seq")
+    private Post post;
 }
