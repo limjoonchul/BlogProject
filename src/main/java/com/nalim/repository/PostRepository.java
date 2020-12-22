d024c836ed4371073549840de3213e6332a24d94
limjoonchul <dlawnscjf511@naver.com>
2020-12-22 16:51:38 +0900

    Add findByTitleContaining Test method  to PostTest

--- src/main/java/com/nalim/repository/PostRepository.java
+++ src/main/java/com/nalim/repository/PostRepository.java
@@ -4,6 +4,17 @@ import com.nalim.model.Member;
 import com.nalim.model.Post;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface PostRepository extends CrudRepository<Post, Long> {
-    // title,
+    // title, content
+
+    // 특정 타이틀
+    List<Post> findByTitle(String title);
+
+    // 특정 타이틀이 들어가 있는 리스트(containing)
+    List<Post> findByTitleContaining(String title);
+
+
 }
--- src/test/java/com/nalim/PostTest.java
+++ src/test/java/com/nalim/PostTest.java
@@ -8,15 +8,20 @@ import com.nalim.repository.BlogRepository;
 import com.nalim.repository.CategoryRepository;
 import com.nalim.repository.MemberRepository;
 import com.nalim.repository.PostRepository;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
 import java.util.Optional;
 
-@RunWith(SpringRunner.class)
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+//@RunWith(SpringRunner.class)
 @SpringBootTest
 public class PostTest {
 
@@ -32,45 +37,55 @@ public class PostTest {
     @Autowired
     private BlogRepository blogRepository;
 
-//    @Test
-    public void insertTest(){
-        Post post = new Post();
-        post.setTitle("포스트테스트");
-        post.setContent("포스트내용테스트");
+    //    @Test
+    public void insertTest() {
+//        Post post = new Post();
+//        post.setTitle("포스트테스트");
+//        post.setContent("포스트내용테스트");
 
 //        Blog blog = new Blog();
 //        blog.setName("블로그테스트");
 //        blog.setDescription("블로그설명테스트");
 
-        Optional<Blog> findBlog = blogRepository.findById(2L);
-        if (findBlog.isPresent()){
-            Blog blog = findBlog.get();
-            post.setBlog(blog);
-        }
+//        Optional<Blog> findBlog = blogRepository.findById(2L);
+//        if (findBlog.isPresent()) {
+//            Blog blog = findBlog.get();
+//            post.setBlog(blog);
+//        }
+//
+//        Optional<Category> findCategory = categoryRepository.findById(1L);
+//        if (findCategory.isPresent()) {
+//            Category category = findCategory.get();
+//            post.setCategory(category);
+//        }
+//
+//        Optional<Member> findMember = memberRepository.findById(1L);
+//        if (findCategory.isPresent()) {
+//            Member member = findMember.get();
+//            post.setMember(member);
+//        }
+//        postRepository.save(post);
 
-        Optional<Category> findCategory = categoryRepository.findById(1L);
-        if (findCategory.isPresent()){
-            Category category = findCategory.get();
-            post.setCategory(category);
-        }
 
-        Optional<Member> findMember =  memberRepository.findById(1L);
-        if (findCategory.isPresent()){
-            Member member = findMember.get();
-            post.setMember(member);
-        }
+        Post post = new Post();
+        post.setTitle("포스트 테스트");
+        post.setContent("포스트 내용 테스트");
         postRepository.save(post);
+
+        Post findPost = postRepository.findById(post.getSeq()).get();
+
+        assertThat(post.getTitle()).isEqualTo(findPost.getTitle());
     }
 
-//    @Test
-    public void updateTest(){
-        Optional<Post> findPost = postRepository.findById(13L);
+    //    @Test
+    public void updateTest() {
+//        Optional<Post> findPost = postRepository.findById(13L);
+//
+//        if (findPost.isPresent()) {
+//            Post post = findPost.get();
+//            post.setTitle("테스트224455");
 
-        if (findPost.isPresent()){
-            Post post = findPost.get();
-            post.setTitle("테스트224455");
-
-            // caseCade를 해야지 가능하지?
+        // caseCade를 해야지 가능하지?
 //            Member member = post.getMember();
 //            member.setName("테스트테스트");
 //            post.setMember(member);
@@ -83,22 +98,72 @@ public class PostTest {
 //            blog.setName("테스트테스트");
 //            post.setBlog(blog);
 
-            postRepository.save(post);
-        }
+//            postRepository.save(post);
+//        }
+
+        Post post = new Post();
+        post.setTitle("포스트 22");
+        post.setContent("포스트 내용 22");
+        postRepository.save(post);
+
+        Post findPost = postRepository.findById(post.getSeq()).get();
+        findPost.setTitle("포스트 33");
+        findPost.setContent("포스트 내용 33");
+        postRepository.save(findPost);
+
+        Post findPost2 = postRepository.findById(findPost.getSeq()).get();
+        assertThat(findPost.getTitle()).isEqualTo(findPost2.getTitle());
 
     }
 
-    @Test
-    public void selectTest(){
+    //    @Test
+    public void selectTest() {
         Optional<Post> findPost = postRepository.findById(1L);
-        if (findPost.isPresent()){
+        if (findPost.isPresent()) {
             System.out.println(findPost.get().getTitle());
             System.out.println(findPost.get().getContent());
         }
     }
 
-//    @Test
-    public void deleteTest(){
+    //    @Test
+    public void deleteTest() {
         postRepository.deleteById(13L);
     }
+
+
+    // 테스트 연습
+//    @BeforeEach
+    public void beforeSetting() {
+
+        for (int i = 0; i < 100; i++) {
+            Post post = new Post(); // 새로운 객체를 생성해서 넣어줘야지 update가 아닌 insert 가 발생하겠지?
+
+            post.setTitle("포스트 " + i);
+            post.setContent("포스트 내용" + i);
+            postRepository.save(post);
+        }
+    }
+
+    @Test
+    public void findByTitle() {
+        Optional<Post> findPost = postRepository.findById(300L);
+
+        if (findPost.isPresent()) {
+            List<Post> postList = postRepository.findByTitle(findPost.get().getTitle());
+            for (Post post1 : postList) {
+                System.out.println(post1.getTitle());
+            }
+        }
+    }
+
+    //    @Test
+    public void findByTitleContaining() {
+        List<Post> postList = postRepository.findByTitleContaining("0");
+        for (Post post : postList) {
+            System.out.println(post.getTitle());
+            assertThat(post.getTitle()).contains("0");
+        }
+    }
+
+
 }
