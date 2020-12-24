86cfb522a8b4f28b53568e3a42269b1a699fa950
limjoonchul <dlawnscjf511@naver.com>
2020-12-24 17:52:14 +0900

    Add findAll method

--- src/main/java/com/nalim/repository/CategoryRepository.java
+++ src/main/java/com/nalim/repository/CategoryRepository.java
@@ -4,6 +4,8 @@ import com.nalim.model.Blog;
 import com.nalim.model.Category;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface CategoryRepository extends CrudRepository<Category, Long> {
-    // name
+    List<Category> findAll();
 }
